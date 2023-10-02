b24c846e0873d6c65c48c924baadb3b27a26c33f
DiMa <francev.dima@gmail.com>
2023-10-02 22:48:07 +0200

    HomeWork

--- index.test.js
+++ index.test.js
@@ -1,34 +1,28 @@
 import { removeElement } from './index.js';
 
-test(
-    'Перевіряємо існуючий елемент видаляється з масиву',
-    () => {
+
+describe("Провірка функції removeElement", () => {
+    test('Перевіряємо існуючий елемент видаляється з масиву', () => {
         const array = [1, 2, 3, 4, 5];
         const itemToRemove = 3;
         const expectedArray = [1, 2, 4, 5];
         expect(removeElement(array, itemToRemove)).toEqual(expectedArray);
-    }
-),
-    test(
-        'спроба видалення неіснуючого елемента не змінює масив',
-        () => {
-            const array = [1, 2, 3, 4, 5];
-            const itemToRemove = 6;
-            const expectedArray = [1, 2, 3, 4, 5];
-            expect(removeElement(array, itemToRemove)).toEqual(expectedArray);
-        }
-    ),
-    test(
-        'для елемента, що повторюється, видаляються всі входження',
-        () => {
-            const array = [1, 2, 2, 3, 4, 2, 5];
-            const itemToRemove = 2;
-            const expectedArray = [1, 3, 4, 5];
-            expect(removeElement(array, itemToRemove)).toEqual(expectedArray);
-        }
-    );
+    });
 
+    test('Спроба видалення неіснуючого елемента не змінює масив', () => {
+        const array = [1, 2, 3, 4, 5];
+        const itemToRemove = 6;
+        const expectedArray = [1, 2, 3, 4, 5];
+        expect(removeElement(array, itemToRemove)).toEqual(expectedArray);
+    });
 
+    test('Для елемента, що повторюється, видаляються всі входження', () => {
+        const array = [1, 2, 2, 3, 4, 2, 5];
+        const itemToRemove = 2;
+        const expectedArray = [1, 3, 4, 5];
+        expect(removeElement(array, itemToRemove)).toEqual(expectedArray);
+    });
+});
 
 
 
